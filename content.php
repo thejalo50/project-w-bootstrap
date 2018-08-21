acc41f673c76f987112722b54980724aee8baa9c
thejalo50 <thejalo50@users.noreply.github.com>
2018-08-21 22:25:43 +0200

    boot

--- content.php
+++ content.php
@@ -1,1 +1,17 @@
+<div class="container">
+
+<div class="row">
+  <div class="col-3">
+    <p>czas</p>
+  </div>
+  <div class="col-3">
+    <p>czas</p>
+  </div>
+  <div class="col-3">
+    <p>czas</p>
+  </div>
+  <div class="col-3">
+    <p>czas</p>
+  </div>
+</div>
 <p>Content</p>
